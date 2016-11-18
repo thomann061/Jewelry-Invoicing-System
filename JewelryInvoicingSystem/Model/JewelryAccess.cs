fa84ee314a9031b18666994021ed7fe86abb9c4f
thomann061 <thomann061@gmail.com>
2016-11-18 09:12:18 -0700

    Add project files.

---
+++ JewelryInvoicingSystem/Model/JewelryAccess.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JewelryInvoicingSystem.Model {
+    class JewelryAccess {
+
+    /// <summary>
+    /// Database Access
+    /// </summary>
+    private DataAccess db;
+    
+    /// <summary>
+    /// Default Constructor
+    /// </summary>
+    public JewelryAccess() {
+            db = new DataAccess();
+        }
+    }
+}
---
+++ JewelryInvoicingSystem/View/DefWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace JewelryInvoicingSystem {
+    /// <summary>
+    /// Interaction logic for DefWindow.xaml
+    /// </summary>
+    public partial class DefWindow : Window {
+        public DefWindow() {
+            InitializeComponent();
+        }
+    }
+}
