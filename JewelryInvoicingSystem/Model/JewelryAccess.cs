c24c644f1b0ea641485bf7a1fae26861c99263cc
thomann061 <thomann061@gmail.com>
2016-11-30 17:19:13 -0700

    Editing SQL statements

--- JewelryInvoicingSystem/Model/JewelryAccess.cs
+++ JewelryInvoicingSystem/Model/JewelryAccess.cs
@@ -32,7 +32,7 @@ namespace JewelryInvoicingSystem.Model {
             string sSQL;    //Holds an SQL statement
             int iRet = 0;   //Number of return values
             DataSet ds = new DataSet(); //Holds the return values
-            sSQL = "SELECT ItemCode, ItemName, ItemDesc, ItemCost FROM Item";
+            sSQL = "SELECT ItemCode, ItemName FROM Item";
             col_Items = new ObservableCollection<Item>();
             try {
                 ds = db.ExecuteSQLStatement(sSQL, ref iRet);
@@ -43,8 +43,6 @@ namespace JewelryInvoicingSystem.Model {
 
                     item.ItemCode = int.Parse(ds.Tables[0].Rows[i]["ItemCode"].ToString());
                     item.ItemName = ds.Tables[0].Rows[i]["ItemName"].ToString();
-                    item.ItemDesc = ds.Tables[0].Rows[i]["ItemDesc"].ToString();
-                    item.ItemCost = double.Parse(ds.Tables[0].Rows[i]["ItemCost"].ToString());
 
                     col_Items.Add(item);
                 }
@@ -73,8 +71,6 @@ namespace JewelryInvoicingSystem.Model {
 
                 item.ItemCode = int.Parse(ds.Tables[0].Rows[0]["ItemCode"].ToString());
                 item.ItemName = ds.Tables[0].Rows[0]["ItemName"].ToString();
-                item.ItemDesc = ds.Tables[0].Rows[0]["ItemDesc"].ToString();
-                item.ItemCost = double.Parse(ds.Tables[0].Rows[0]["ItemCost"].ToString());
 
                 col_Items.Add(item);
             } catch (Exception e) {
@@ -150,8 +146,8 @@ namespace JewelryInvoicingSystem.Model {
         public bool insertItem(Item item) {
             string sSQL;    //Holds an SQL statement
             int rowCount = 0;   //Number of rows Affected
-            sSQL = "INSERT INTO Item (ItemName, ItemDesc, ItemCost) " +
-                   "VALUES(" + item.ItemName + ", " + item.ItemDesc + ", " + item.ItemCost + ")";
+            sSQL = "INSERT INTO Item (ItemName, ItemCost) " +
+                   "VALUES(" + item.ItemName + ")";
             try {
                 rowCount = db.ExecuteNonQuery(sSQL);
                 //if insert unsuccessful
@@ -167,22 +163,26 @@ namespace JewelryInvoicingSystem.Model {
         /// <summary>
         /// SQL Statement that inserts an invoice
         /// </summary>
-        /// <returns>true if successful or false if unsuccessful</returns>
-        public bool insertInvoice(Invoice invoice) {
+        /// <returns>id if success, otherwise 0</returns>
+        public int insertInvoice(Invoice invoice) {
             string sSQL;    //Holds an SQL statement
+            string idSQL;    //Holds an SQL statement
             int rowCount = 0;   //Number of rows Affected
+            int newId = 0;
             sSQL = "INSERT INTO Invoice (InvoiceDate) " +
-                   "VALUES(" + invoice.InvoiceDate + ")";
+                   "VALUES('" + invoice.InvoiceDate + "');";
+            idSQL = "SELECT TOP 1 InvoiceCode FROM Invoice ORDER BY InvoiceCode DESC;";
             try {
                 rowCount = db.ExecuteNonQuery(sSQL);
+                newId = int.Parse(db.ExecuteScalarSQL(idSQL));
                 //if insert unsuccessful
                 if (rowCount == 0)
-                    return false;
+                    return newId;
             } catch (Exception e) {
                 Console.WriteLine("{0} Exception caught.", e);
             }
             //if successful
-            return true;
+            return newId;
         }
 
         //End Insert Statements
@@ -197,7 +197,7 @@ namespace JewelryInvoicingSystem.Model {
             string sSQL;    //Holds an SQL statement
             int rowCount = 0;   //Number of rows Affected
             sSQL = "UPDATE Item " +
-                   "SET ItemName=" + item.ItemName + ", ItemDesc=" + item.ItemDesc + ", ItemCost=" + item.ItemCost + " " +
+                   "SET ItemName=" + item.ItemName + ", ItemCost=" + " " +
                    "WEHERE ItemCode = " + item.ItemCode;
             try {
                 rowCount = db.ExecuteNonQuery(sSQL);
@@ -212,20 +212,33 @@ namespace JewelryInvoicingSystem.Model {
         }
 
         /// <summary>
-        /// SQL Statement that updates an invoice
+        /// SQL Statement that updates an invoice and its items
         /// </summary>
         /// <returns>true if successful or false if unsuccessful</returns>
-        public bool updateInvoice(Invoice invoice) {
+        public bool updateInvoiceWithItems(ObservableCollection<Invoice> invoices, ObservableCollection<InvoiceItem> invoiceItems) {
+            //there should only be one invoice in the array
+            Invoice invoice = invoices.ElementAt(0);
             string sSQL;    //Holds an SQL statement
+            string s2SQL;    //Holds an SQL statement
             int rowCount = 0;   //Number of rows Affected
             sSQL = "UPDATE Invoice " +
-                   "SET InvoiceDate=" + invoice.InvoiceDate + " " +
-                   "WEHERE InvoiceCode = " + invoice.InvoiceCode;
+                   "SET InvoiceDate='" + invoice.InvoiceDate + "' " +
+                   "WHERE InvoiceCode = " + invoice.InvoiceCode + ";";
             try {
                 rowCount = db.ExecuteNonQuery(sSQL);
                 //if insert unsuccessful
                 if (rowCount == 0)
                     return false;
+
+                //insert items
+                foreach (InvoiceItem el in invoiceItems) {
+                    s2SQL = "INSERT INTO InvoiceItem (ItemCode, InvoiceCode, ItemPrice)" +
+                            "VALUES(" + el.Item.ItemCode + ", " + invoice.InvoiceCode + ", " + el.ItemCost + ");";
+                    rowCount = db.ExecuteNonQuery(s2SQL);
+                    //if insert unsuccessful
+                    if (rowCount == 0)
+                        return false;
+                }
             } catch(Exception e) {
                 Console.WriteLine("{0} Exception caught.", e);
             }
